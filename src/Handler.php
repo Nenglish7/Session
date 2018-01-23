3ef6fbf6eb4340fcb6f561cdb6584e7ee822703b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-23 13:26:41 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -16,4 +16,34 @@ namespace Nenglish7\Session;
  */
 class Handler extends Manager implements HandlerInterface
 {
+    /**
+     * set().
+     *
+     * Set a session variable.
+     *
+     * @param string $name The session variable name.
+     * @param mixed $value The value that is to be set.
+     *
+     * @throws DomainException          If the `$name` argument is not valid.
+     * @throws InvalidArgumentException If the `$value` argument is an object.
+     * @throws RuntimeException         If the session is not active.
+     * @throws InvalidArgumentException If the `$value` argument is null.
+     *
+     * @return void.
+     */
+    public function set(string $name, $value = null): void
+    {
+        $name = trim($name);
+        if ($name == '' || empty($name)) {
+            throw new Exception\DomainException('The `$name` argument is not valid.');
+        } elseif (is_object($value)) {
+            throw new Exception\InvalidArgumentException('The `$value` argument is an object.');
+        } elseif ($this->sessionActive == false) {
+            throw new Exception\RuntimeException('The session is not active.');
+        } elseif (is_null($value) || $value === null) {
+            throw new Exception\InvalidArgumentException('The `$value` argument is null.');
+        } else {
+            $this->run('set', $name, $value);
+        }
+    }
 }
