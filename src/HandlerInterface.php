c56f4d163e0ef537effc078f1e7c8e36688e6045
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-23 12:31:16 -0500

    Update HandlerInterface.php

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,1 +1,19 @@
-
+<?php
+/**
+ * This file is a part of Nenglish7/Session.
+ *
+ * @author Nicholas English               <https://github.com/Nenglish7>.
+ * @author Nenglish7/Session Contributors <https://github.com/Nenglish7/Session/graphs/contributors>.
+ *
+ * @copyright 2017-2018 Nenglish7.
+ *
+ * @link    <https://github.com/Nenglish7/Session> Github Repository.
+ * @license <https://github.com/Nenglish7/Session/blob/master/LICENSE> New BSD License.
+ */
+namespace Nenglish7\Session;
+/**
+ * HandlerInterface.
+ */
+interface HandlerInterface
+{
+}
